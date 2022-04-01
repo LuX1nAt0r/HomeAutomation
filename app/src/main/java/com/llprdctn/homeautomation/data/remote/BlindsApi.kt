f555ee9e4867a04b1cd367fc82469facb4d65b61
Lukas <l.landsteiner@t-online.de>
2022-04-01 21:47:38 +0200

    Added Database and other stuff related to saving and requesting BlindsStates, but need to finish the repository

--- app/src/main/java/com/llprdctn/homeautomation/data/remote/BlindsApi.kt
+++ app/src/main/java/com/llprdctn/homeautomation/data/remote/BlindsApi.kt
@@ -1,11 +1,19 @@
 package com.llprdctn.homeautomation.data.remote
 
 import com.llprdctn.homeautomation.data.local.entities.Blind
+import com.llprdctn.homeautomation.data.remote.responses.SimpleResponse
+import okhttp3.ResponseBody
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface BlindsApi {
 
     @GET("/getCurrentStates")
     suspend fun getCurrentStates(): Response<List<Blind>>
+
+
+    @POST("/updateCurrentStates")
+    suspend fun updateCurrentStates(@Body blind: Blind): Response<ResponseBody>
 }
--- app/src/main/java/com/llprdctn/homeautomation/repositories/Repository.kt
+++ app/src/main/java/com/llprdctn/homeautomation/repositories/Repository.kt
@@ -1,9 +1,11 @@
 package com.llprdctn.homeautomation.repositories
 
+import android.app.Application
 import com.llprdctn.homeautomation.data.local.BlindDao
 import com.llprdctn.homeautomation.data.remote.BlindsApi
 import com.llprdctn.homeautomation.data.local.entities.Blind
 import com.llprdctn.homeautomation.other.Resource
+import com.llprdctn.homeautomation.other.checkForInternetConnection
 import com.llprdctn.homeautomation.other.networkBoundResource
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
@@ -11,8 +13,23 @@ import javax.inject.Inject
 
 class Repository @Inject constructor(
     private val blindsApi: BlindsApi,
-    private val blindDao: BlindDao
+    private val blindDao: BlindDao,
+    private val context: Application
 ) {
+
+
+    suspend fun insertBlind(blind: Blind) {
+        val response = try {
+            blindsApi.updateCurrentStates()
+        }// TODO: HERE---------------------------------------------------------------
+    }
+
+
+
+
+    suspend fun insertBlinds(blinds: List<Blind>) {
+        blinds.forEach{ insertBlind(it)}
+    }
 
 
     fun getCurrentBlindsState(): Flow<Resource<List<Blind>>> {
@@ -25,12 +42,12 @@ class Repository @Inject constructor(
             },
             saveFetchResult = { response ->
                 response.body()?.let {
-
+                    insertBlinds(it.onEach {  })
                 }
 
             },
             shouldFetch = {
-                //TODO: Here
+                checkForInternetConnection(context)
             }
 
         )
